cb7f80ab7c2d1017a58e787ede07a76ac28c3e2d
MateusPMG <mpmgboss@gmail.com>
2023-11-07 16:15:09 +0000

    working on it

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,73 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:09:31 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/11/07 14:09:32 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/11/07 16:05:49 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+Account::Account(void)
+{
+	//constructs account class type object;
+}
+
+Account::Account(int initial_deposit)
+{
+	//constructs new account
+}
+
+Account::~Account(void)
+{
+	//destructor
+}
+
+int	Account::getNbAccounts(void)
+{
+	
+}
+
+int	Account::getTotalAmount(void)
+{
+	
+}
+
+int	Account::getNbDeposits(void)
+{
+	
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	
+}
+
+int		Account::checkAmount(void) const
+{
+	
+}
+
+void	Account::displayStatus(void) const
+{
+	
+}
+
+void	Account::_displayTimestamp(void)
+{
+	
+}

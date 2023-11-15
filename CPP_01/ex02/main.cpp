abcc01c9fb046bf65f3b5da38ca6a736f58ae3ab
MateusPMG <mpmgboss@gmail.com>
2023-11-15 15:10:38 +0000

    going

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:37:27 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/11/15 15:05:31 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/11/15 15:10:27 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,4 @@ int	main(void)
 	std::cout << "value of string: " << string << std::endl;
 	std::cout << "value pointed to by ptr: " << *ptr << std::endl;
 	std::cout << "value pointed to by ref: " << ref << std::endl;
-	
 }

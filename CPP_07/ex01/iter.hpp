58d8acb37669f1712865b41c20501566b3d0ae6b
MateusPMG <mpmgboss@gmail.com>
2024-01-23 16:27:20 +0000

    done ex01

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -6,11 +6,27 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:21:26 by mpatrao           #+#    #+#             */
-/*   Updated: 2024/01/23 14:29:26 by mpatrao          ###   ########.fr       */
+/*   Updated: 2024/01/23 16:24:20 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
 
-template <typename addr, unsigned int length, typename func>
-void iter()
+template <typename T>
+void pos(T &pos){
+    std::cout << "array has: " << pos << std::endl;
+}
+
+
+template <typename addr>
+void iter(addr *array, unsigned int length, void (*function)(addr &)){
+    for (unsigned int i = 0; i < length; i++)
+        function(array[i]);
+}
+
+template <typename addr>
+void iter(const addr *array, unsigned int length, void (*function)(addr &)){
+    for (unsigned int i = 0; i < length; i++)
+        function(array[i]);
+}
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -6,7 +6,25 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:21:15 by mpatrao           #+#    #+#             */
-/*   Updated: 2024/01/23 14:21:16 by mpatrao          ###   ########.fr       */
+/*   Updated: 2024/01/23 16:26:13 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+
+int main(void){
+    int array[] = {1, 2, 3, 4, 5, 6};
+    
+    ::iter(array, 6, pos<int>);
+    
+    char array2[] = {'a', 'b' , 'c'};
+
+    ::iter(array2, 3, pos<char>);
+
+    std::string array3[] = {"olha", "e assim"};
+
+    ::iter(array3, 2, pos<std::string>);
+    
+    return (0);
+}
